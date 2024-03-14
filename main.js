fc72f904014ff42af809ba326dc3a22a8b0ed589
Safdar420 <160203243+Safdar420@users.noreply.github.com>
2024-03-14 12:18:12 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+//  Q no 19
+var Guests = ["Amir", "Adil", "Abdullah", "Wakeel", "Raheel", "Jawwad"];
+console.log("I am inviting people ".concat(Guests.length, " to dinner"));
+//  Q no 20
+var FavouriteProgramminglanguages = ["JAVASCRIPT", "TYPESCRIPT", "PYTHON", "HTML", "CSS"];
+FavouriteProgramminglanguages.forEach(function (FavouriteProgramminglanguages) {
+    console.log("".concat(FavouriteProgramminglanguages, " is my favourite programming language"));
+});
+//  Q no 21
+var myCar = {
+    name: "COROLLA",
+    model: "2024",
+    company: "XLI"
+};
+console.log(myCar);
---
+++ main.ts
@@ -0,0 +1,18 @@
+//  Q no 19
+let Guests :string[] =["Amir","Adil","Abdullah","Wakeel","Raheel","Jawwad"];
+console.log(`I am inviting people ${Guests.length} to dinner`);
+
+//  Q no 20
+let FavouriteProgramminglanguages :string[] =["JAVASCRIPT","TYPESCRIPT","PYTHON","HTML","CSS"];
+FavouriteProgramminglanguages.forEach(FavouriteProgramminglanguages => {
+    console.log(`${FavouriteProgramminglanguages} is my favourite programming language`);
+    
+});
+
+//  Q no 21
+let myCar = {
+    name :"COROLLA",
+    model :"2024",
+    company :"XLI"
+}
+console.log(myCar);
